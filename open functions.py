7dd2ca2e2669eb1fc62b22a3ec5b4664a01729cf
SimarpreetAneja <simarpreetkaur2844@gmail.com>
2022-08-09 19:13:56 +0530

    first commit

---
+++ open functions.py
@@ -0,0 +1,17 @@
+
+#t=open('demo','w+')
+#t.write('simar')
+#print(t.read())
+
+#print("hello")
+#f=open("demo","r")
+#print(f.read())
+
+
+#t=open("demo","a+")
+#t.write("raman")
+#print(t.read))
+
+
+t=open("demo","w")
+t.writelines(["taran\n","ishu\n","mehar\n"])
